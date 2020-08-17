cecca16e40156a90553d47fd03a34fe3cd56e5be
Felipe delmondes <luis.delmondes@outlook.com>
2020-08-17 16:50:34 -0300

    Quarto Commit, alterando arquivo teste para Menu, criando conexxões com o banco de dados

---
+++ menu.py
@@ -0,0 +1,47 @@
+from produto import Produto
+import pymysql.cursors
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='Amil@2020',
+                             db='estoque',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+print ('Controle de estoque')
+
+print('Digite: 1-Cadastro 2-Consulta\n')
+
+escolha = int(input("Escolha: "))
+
+if (escolha == 1):
+    tipo = input("Qual o tipo do produto: ")
+    genero = input("Qual o gênero do produto: ")
+    tamanho = input('Qual o tamanho: ')
+    cor = input('Digite a cor: ')
+    marca = input('Qual a marca: ')
+    estoq_var = input('Qual a quantidade: ')
+
+    prod1 = Produto(tipo,genero,tamanho,cor,marca,estoq_var)
+
+    with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `produtos` (`tipo`, `genero`,`tamanho`,`marca`,`cor`,`quantidade`) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (prod1.tipo,prod1.genero,prod1.tamanho,prod1.marca,prod1.cor,prod1.quantidade))
+
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+    connection.commit()
+
+
+    print ('Cadastro efetuado com sucesso')
+elif (escolha ==2):
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM produtos WHERE `quantidade`>%s"
+        cursor.execute(sql, ('0',))
+        for  item in cursor.fetchall():
+            print(item)
+
+    connection.close()
--- produto.py
+++ produto.py
@@ -1,7 +1,8 @@
 class Produto:
-    def __init__(self, tipo,genero,tamanho,marca,quantidade):
+    def __init__(self, tipo,genero,tamanho,cor,marca,quantidade):
         self.tipo = tipo
         self.genero = genero
         self.tamanho = tamanho
+        self.cor = cor
         self.marca = marca
-        self.quantidade = input('Digite a quantidade: ')
+        self.quantidade = quantidade
